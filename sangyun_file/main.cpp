dfb47d802d8d46e3a9c50f1d118735c34e368df8
Park-Sangyun <85819413+Park-Sangyun@users.noreply.github.com>
2022-06-15 15:28:04 +0900

    Update main.cpp

--- sangyun_file/main.cpp
+++ sangyun_file/main.cpp
@@ -1,54 +1,44 @@
 #include "snakeGame.h"
 
 void playGame();
-int ready();
-int AskUserToPlayAgain();
-void ClearCentre();
-int UserInput();
+
+int input()
+{
+	int input = getch();
+	refresh();
+	endwin();
+	clear();
+
+	return input;
+}
+
+int ready()
+{
+  initscr();
+	noecho();
+	move(10, 1);
+	printw("Are you ready? (y/n)");
+	return input();
+}
+
+int playAgain()
+{
+	move(10, 6);
+	printw("try again?");
+  move(11, 3);
+  printw("     (y/n)     ");
+	return input();
+}
 
 int main ()
 {
 	if (ready() == 'y')
 	do {
 		{
-			snakeGame NewSnake;
-			NewSnake.playGame();
+			snakeGame game;
+			game.playGame();
 		}
 	}
-	while (AskUserToPlayAgain() == 'y');
+	while (playAgain() == 'y');
 	return 0;
-}
-
-void ClearCentre(float x, float y)
-{
-	clear();
-	initscr();
-	noecho();
-	move(x, y);
-}
-
-int UserInput()
-{
-	int UserInput = getch();
-	refresh();
-	endwin();
-	clear();
-
-	return UserInput;
-}
-
-int ready()
-{
-	ClearCentre(10, 1);
-	printw("Are you ready? (y/n)");
-	return UserInput();
-}
-
-int AskUserToPlayAgain()
-{
-	move(10, 6);
-	printw("try again?");
-  move(11, 3);
-  printw("     (y/n)     ");
-	return UserInput();
 }
