5d44a66c5fbfbc26f426c714506d22b7acdd29b9
Park-Sangyun <85819413+Park-Sangyun@users.noreply.github.com>
2022-06-15 14:42:09 +0900

    Update and rename fsnakegame.h to snakeGame.h

---
+++ sangyun_file/snakeGame.h
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <ncurses.h>
+#include <unistd.h>
+#ifndef SNAKEGAME_H
+#define SNAKEGAME_H
+
+struct position
+{
+	int x, y;
+	position(int col, int row);
+	position();
+};
+
+class snakeGame
+{
+private:
+	int nowsize = 3, bestsize = 3, wall = 21, fruitcnt = 0, poisoncnt = 0;
+	char direction, head, body, edgechar, fruitshape, poisonshape;
+	bool eatFruit;
+	bool eatPoison;
+	position fruit;
+	position poison;
+	std::vector<position> snake;
+
+	void InitGameWindow();
+	void DrawWindow();
+	void DrawSnake();
+	void PrintScore();
+	void PrintMission();
+	void PositionFruit();
+	void PositionPoison();
+	bool fail();
+	void MoveSnake();
+	bool GetsFruit();
+	bool GetsPoison();
+
+public:
+	snakeGame();
+	~snakeGame();
+	void playGame();
+};
+
+#endif
